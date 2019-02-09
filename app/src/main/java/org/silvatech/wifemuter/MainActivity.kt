b9c68d014bb9b74acf8c1c20b0d5e4835f0a6d36
csilva <csilva@silvatech.org>
2019-02-09 16:42:41 -0600

    Added Buttons with wall paper to layout for main activity and also designs on the button. Media player was extended to audio resources with private init var. Buttons assigned to val then onclick for val with if logic. Also AdMod Feature was added to application with implementation in gradle and key in manifest, followed by initialization the library in the main activity.  Then banner was added to main layout as well. Resources were also added. Git ignore was also updated.

--- app/src/main/java/org/silvatech/wifemuter/MainActivity.kt
+++ app/src/main/java/org/silvatech/wifemuter/MainActivity.kt
@@ -1,12 +1,52 @@
 package org.silvatech.wifemuter
 
+import android.media.MediaPlayer
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.ads.AdRequest
+import kotlinx.android.synthetic.main.activity_main.*
+import com.google.android.gms.ads.MobileAds;
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var audio1: MediaPlayer
+    private lateinit var audio2: MediaPlayer
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        MobileAds.initialize(this, "ca-app-pub-9497317358583649~7226564155")
+        adView.loadAd(AdRequest.Builder().build())
+
+        audio1 = MediaPlayer.create(this, R.raw.nice_quiet)
+
+        audio2 = MediaPlayer.create(this, R.raw.naughty_quiet)
+
+        val buttonNice = niceBtn
+
+        val buttonNaughty = naughtyBtn
+
+
+
+        buttonNice.setOnClickListener {
+            if (audio2.isPlaying == true) {
+
+            } else {
+                audio1.start()
+            }
+
+
+        }
+
+        buttonNaughty.setOnClickListener {
+            if (audio1.isPlaying == true) {
+
+            } else {
+                audio2.start()
+            }
+
+
+        }
     }
 }
